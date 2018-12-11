8f67d76cb3b18749b8c237471d701d9332ef0801
Jamie Cox <me@jcox.tech>
2018-12-11 08:17:26 +0000

    Scrape stream for iTunes

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import os
 
 import flask
 import iso8601
+import lxml.html as lxh
 import requests
 from flask_cacheify import init_cacheify
 
@@ -13,13 +14,16 @@ app.debug = os.environ.get('DEBUG', False)
 app.cache = init_cacheify(app)
 app.timeout_mins = int(os.environ.get('CACHE_TIMEOUT_MINS', 10))
 app.download_url = os.environ.get('MIXCLOUD_DOWNLOAD_URL', '')
+app.max_items = int(os.environ.get('MAX_ITEMS', 10))
 mixcloud_user_url = os.environ.get('MIXCLOUD_USER_URL', 'https://api.mixcloud.com/riffwizzards/')
 mixcloud_cast_url = os.environ.get('MIXCLOUD_CAST_URL', 'https://api.mixcloud.com/riffwizzards/cloudcasts/')
 
 
 @app.template_filter('get_full_download_url')
 def episode_url(value):
-    return f"{app.download_url}{value}"
+    response = requests.get(f'{app.download_url}{value}')
+    html = lxh.fromstring(response.text)
+    return html.cssselect('div p a')[-1].text
 
 
 @app.template_filter('parse_date')
@@ -31,7 +35,7 @@ def parse_8601_2822(value):
 @app.cache.cached(timeout=1 if app.debug else 60 * app.timeout_mins)
 def feed():
     rss_xml = flask.render_template('rss.xml',
-                                    items=requests.get(mixcloud_cast_url).json()['data'],
+                                    items=requests.get(mixcloud_cast_url).json()['data'][:app.max_items],
                                     user=requests.get(mixcloud_user_url).json(),
                                     now=email.utils.format_datetime(datetime.datetime.utcnow()),
                                     )
